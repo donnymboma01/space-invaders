6ebf3a5748a93434c28c27c862bcbd62b4dfeadf
g49829 <49829@etu.he2b.be>
2018-04-19 13:39:40 +0200

    deplacement Vaisseau

--- Vaisseau.js
+++ Vaisseau.js
@@ -1,41 +1,54 @@
 // Classe représentant le vaisseau
-class Vaisseau{
+class Vaisseau {
 
     // Constructeur
-    constructor(img){
-        this.posX = 320;
+    constructor(img) {
+        this.posX = 40;
         this.posY = 200;
         this.img = img;
-        // A compléter
+
     }
 
-    get posY(){
+    get posY() {
         return this._posY;
     }
 
-    set posY(value){
+    set posY(value) {
         this._posY = value;
     }
 
-    get posX(){
+    get posX() {
         return this._posX;
     }
 
-    set posX(value){
+    set posX(value) {
         this._posX = value;
     }
 
 
     // Initialise le vaisseau dans le html
-    initHtml(){
+    initHtml() {
         var vaisseau = document.getElementById('vaisseau');
         vaisseau.src = this.img;
         vaisseau.style.position = "absolute";
-        vaisseau.style.top = this.posY+"px";
-        vaisseau.style.left = this.posX+"px";
+        vaisseau.style.top = this.posY + "px";
+        vaisseau.style.left = this.posX + "px";
     }
     dislay() {
-        initHtml();
-    }
+        var vaisseau = document.getElementById('vaisseau');
+        vaisseau.src = this.img;
+        vaisseau.style.top = this.posY + "px";
+        vaisseau.style.left = this.posX + "px";
 
+    }
+    //cette fonction permet de bouger le vaisseau en tenant compte du bouton appuyé vers le haut et vers le bas.
+    //le vaisseau ne doit pas depasser le cadre.
+
+    move(distance) {
+        if (this.posY + distance > 0 && this.posY + distance < 400) {
+            this.posY += distance;
+            this.dislay();
+
+        }
+    }
 }
--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 
-//on afffiche le vaisseau su le champ de batail,pour mon cas ca sera un petit vaisseau.
+//on afffiche le vaisseau su le champ de bataille,pour mon cas ca sera un petit vaisseau.
 
 var afficherLeVaisseaux = new Vaisseau("images/vaisseau-ballon-petit.png");
 afficherLeVaisseaux.initHtml();
+
+//la foncttion permet de deplacer le vaisseau sur le champ de bataille.
+
+function affKeyCode(event) {
+    var key = event.keyCode;
+    if (key==38){
+        afficherLeVaisseaux.move(-20);
+    }
+    if(key==40){
+        afficherLeVaisseaux.move(20);
+    }
+}
